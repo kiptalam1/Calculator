35a1b6017dc70324852c66618df3d12ad885ae27
adams <adamskiptalam0@gmail.com>
2024-10-24 19:00:39 +0300

    Create variables for first number, operator, and last number.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+let first_num;
+let second_num;
+let operator;
+
 function add(num1, num2) {
     return num1 + num2;
 }
 
-// console.log(add(2, 3));
 function subtract(num1, num2) {
     return num1 - num2;
 }
@@ -10,3 +13,4 @@ function subtract(num1, num2) {
 const multiply = (num1, num2) => num1 * num2;
 
 const divide = (num1, num2) => num1 / num2;
+

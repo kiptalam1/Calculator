3f0428648caf373bd07636fae19623565e0071dc
adams <adamskiptalam0@gmail.com>
2024-10-24 19:06:51 +0300

    Correct divde function to handle zero division error.

--- script.js
+++ script.js
@@ -12,5 +12,13 @@ function subtract(num1, num2) {
 
 const multiply = (num1, num2) => num1 * num2;
 
-const divide = (num1, num2) => num1 / num2;
+function divide (num1, num2) {
+    if (num2 === 0) {
+        return "Error: Division by zero";
+    }
+    return num1 / num2;
+}
 
+// function operate(operator, num1, num2) {
+
+// }
